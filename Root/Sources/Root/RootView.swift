a1a38cea52cf5b08ea8da40541d82eaf003cb026
Jinno-Shigeki <booboo080yamaya@gmail.com>
2025-04-06 19:31:52 +0900

    clean first created file

--- Root/Sources/Root/RootView.swift
+++ Root/Sources/Root/RootView.swift
@@ -9,48 +9,10 @@ import SwiftUI
 import SwiftData
 
 public struct RootView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
 
     public var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+        NavigationStack {
+            Text("first view")
         }
     }
     
@@ -59,5 +21,4 @@ public struct RootView: View {
 
 #Preview {
     RootView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
